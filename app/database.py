7e587771f35cc5a667b1885dbe3f33e51f298607
Prabha S <prabhasp1329@gmail.com>
2026-02-26 01:52:32 +0530

    Initial commit

--- app/database.py
+++ app/database.py
@@ -1,13 +1,29 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./procurement.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+# 🔥 If running locally (no env variable)
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./procurement.db"
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    # 🔥 Render PostgreSQL needs this fix
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace(
+            "postgres://", "postgresql://", 1
+        )
+
+    engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
